7ab7ff30d0ba390252baa3745600ad1729530355
Brandon <brandon.se2009@gmail.com>
2021-09-11 04:21:59 -0400

    Initial Commit

---
+++ TareaCalculadoraSwiftUI/TareaCalculadoraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TareaCalculadoraSwiftUIApp.swift
+//  TareaCalculadoraSwiftUI
+//
+//  Created by user203110 on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TareaCalculadoraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
